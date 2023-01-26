c19caf2fcf349511bdee8255e0961e44c1efed79
MimiSuri <miimiisurii@yahoo.com>
2023-01-26 11:44:22 -0800

    update

--- main.h
+++ main.h
@@ -28,7 +28,7 @@ struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
-}
+};
 
 
 /**
@@ -113,6 +113,7 @@ int is_digit(char);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
